792b0001390b83e1203ce082260ed3b707f4c16c
A18yan <ma18yan@gmail.com>
2023-10-06 15:47:11 +0545

    address-book-crud

--- controllers/address-book.controller.js
+++ controllers/address-book.controller.js
@@ -23,3 +23,64 @@ export const createAddressBook = async (req, res) => {
     });
   }
 };
+
+export const getAddressBook = async (req, res) => {
+  try {
+    const addressBook = await AddressBook.find({
+      user: req.user._id,
+    });
+    if (!addressBook) return createError(404, "Address book not found");
+    res.status(200).json({
+      status: "success",
+      address: addressBook,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+
+// delete
+export const deleteAddressBook = async (req, res) => {
+  try {
+    const addressBook = await AddressBook.findByIdAndDelete(req.params.id);
+    if (!addressBook) return createError(404, "Address book not found");
+    res.status(200).json({
+      status: "success",
+      message: "Address book deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+
+// update
+export const updateAddressBook = async (req, res) => {
+  const { city, province, area } = req.body;
+  try {
+    const addressBook = await AddressBook.findByIdAndUpdate(
+      req.params.id,
+      {
+        city,
+        province,
+        area,
+      },
+      { new: true }
+    );
+    if (!addressBook) return createError(404, "Address book not found");
+    res.status(200).json({
+      status: "success",
+      message: "Address book updated successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
--- routes/address-book.routes.js
+++ routes/address-book.routes.js
@@ -2,12 +2,22 @@ import express from "express";
 const router = express.Router();
 
 import { validateCreateAddressBook } from "../validators/address-book.validator.js";
-import { createAddressBook } from "../controllers/address-book.controller.js";
+import {
+  createAddressBook,
+  getAddressBook,
+  deleteAddressBook,
+  updateAddressBook,
+} from "../controllers/address-book.controller.js";
 import { protect } from "../middlewares/auth.js";
 import { validate } from "../middlewares/validate.js";
 
 router
   .route("/")
-  .post(protect, validateCreateAddressBook, validate, createAddressBook);
+  .post(protect, validateCreateAddressBook, validate, createAddressBook)
+  .get(protect, getAddressBook);
 
+router
+  .route("/:id")
+  .delete(protect, deleteAddressBook)
+  .put(protect, updateAddressBook);
 export default router;
